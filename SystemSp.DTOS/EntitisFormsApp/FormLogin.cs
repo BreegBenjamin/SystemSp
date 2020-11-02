6a7eed6a6ee5aa6c3c3970cfc31e7ae4daede764
BreegBenjamin <ernestocajar@outlook.com>
2020-11-02 10:29:16 -0500

    Actualizando el formulario de inicio de sesión

--- SystemSp.DTOS/EntitisFormsApp/FormLogin.cs
+++ SystemSp.DTOS/EntitisFormsApp/FormLogin.cs
@@ -1,8 +1,16 @@
-﻿namespace SystemSp.DTOS.EntitisFormsApp
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SystemSp.DTOS.EntitisFormsApp
 {
     public class FormLogin
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string EmailLogin { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string PasswordLogin { get; set; }
     }
 }
